f309301e9234e29aae356e78c6e97b569f6f3c97
Akil Kumar Thota <akthota@microsoft.com>
2019-04-10 15:46:45 -0700

    Initial Commit

---
+++ Instagram-Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Akil Kumar Thota on 4/10/19.
+//  Copyright © 2019 Akil Kumar Thota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
